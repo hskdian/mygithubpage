24e5f9df98a8aa3269d8e1843cd5cbf86162edb6
hskdian <ian.yiyuan.lin@gmail.com>
2016-12-20 16:22:11 -0800

    finished javascript arrays

--- 11-Javascript Arrays/list.js
+++ 11-Javascript Arrays/list.js
@@ -3,12 +3,33 @@ var input = prompt ("What would you like to do?");
 
 while (input !== "quit"){
   if (input === "list") {
-    console.log(todos);
+    listTodos();
   }
   else if (input === "new"){
-    var newTodo = prompt("Enter new todo");
-    todos.push(newTodo);
+    newTodos();
+  }
+  else if (input === "delete"){
+    deleteTodos();
   }
   input = prompt ("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos(){
+  todos.forEach(function(todo, i){
+    console.log("**********");
+    console.log(i + ": " + todo);
+  });
+}
+
+function newTodos(){
+  var newTodo = prompt("Enter new todo");
+  todos.push(newTodo);
+  console.log("Aadded Todo");
+}
+
+function deleteTodos(){
+  var index = prompt("Enter index of todo to delete");
+  todos.splice(index,1);
+  console.log("Deleted Todo");
+}
---
+++ 11-Javascript Arrays/problem.js
@@ -0,0 +1,35 @@
+function printReverse(array){
+  for(var i = array.length -1;i>=0;i--){
+    console.log(array[i]);
+  }
+}
+
+function isUniform(arr){
+  var first = arr[0];
+  for(var i=1;i<arr.length;i++){
+    if(first!==arr[i]){
+      console.log("Not Uniform!!!");
+      return false;
+    }
+  }
+  console.log("Uniform!!!");
+  return true;
+}
+
+function sumArray(arr){
+  var total = 0;
+  arr.forEach(function(item){
+    total+=item;
+  });
+  return total;
+}
+
+function max(){
+  var max = arr[0];
+  for(var i=1;i<arr.length;i++){
+    if (max<arr[i]){
+      max=arr[i];
+    }
+  }
+  return max;
+}
